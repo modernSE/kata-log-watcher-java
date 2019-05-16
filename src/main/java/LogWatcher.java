cb6f57a16afd869bdf33cf9bd7023012485d3068
modernSE Trainee 4 <keydon+mse-trainee@gmail.com>
2019-05-16 16:42:21 +0200

    Ergebnis der Gruppe 4

--- src/main/java/LogWatcher.java
+++ src/main/java/LogWatcher.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -5,20 +9,39 @@ import java.util.Optional;
  */
 public class LogWatcher {
 
-    private static final String[] subscribers = {"Robert Glaser", "Britta Glatt", "Michael GrÃ¼n"};
-
+    private static final String[] allSubscribers = {"Robert Glaser", "Britta Glatt", "Michael Grün", "Antonio Materazzo", "Fritz Schnitzel"};
+    private static final String[] robertGlaser = {"Robert Glaser"};
+    
+    List<String> logEntries = new ArrayList<>();
+        
     public void watchAndAlert() {
         Optional<String> logEntry = Log.popNextLine();
-        logEntry.ifPresent(this::notifySubscribers);
+        if (!logEntry.isPresent()) {
+        	return;
+        }
+        logEntries.add(logEntry.get());
+        if (logEntry.get().equals("An error occured")) {
+        	notifySubscribers(logEntry.get(), allSubscribers);
+        }
+        if (logEntry.get().equals("ExceptionCode 42")) {
+        	String lastTraceOutput = "";
+        	for (String lastLogEntry : logEntries) {
+        		if (lastLogEntry.equals("Trace output")) {
+        			lastTraceOutput = lastLogEntry;
+        		}
+           	}
+        	notifySubscribers(logEntry.get(), robertGlaser);
+        	notifySubscribers(lastTraceOutput, robertGlaser);
+        }
     }
 
-    private void notifySubscribers(String logMessage) {
+    private void notifySubscribers(String logMessage, String [] subscribers) {
         for (int i = 0; i < subscribers.length; i++) {
             String name = subscribers[i];
             name = name.toLowerCase();
-            name.replace("Ã¼", "ue");
-            name.replace("Ã¤", "ae");
-            name.replace("Ã¶", "oe");
+            name.replace("ü", "ue");
+            name.replace("ä", "ae");
+            name.replace("ö", "oe");
             name.replace(" ", ".");
             name = name + "@cas.de";
 
--- src/test/java/LogWatcherTest.java
+++ src/test/java/LogWatcherTest.java
@@ -1,3 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,9 +14,16 @@ import org.junit.Test;
 public class LogWatcherTest {
 
     @Test
-    public void testLogWatcher() {
+    public void testLogWatcher() throws InterruptedException {
         LogWatcher logWatcher = new LogWatcher();
-        logWatcher.watchAndAlert();
-        Assert.fail();
+//        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//        System.setOut(new PrintStream(outContent));
+        for (int i = 0; i < 50; i++) {
+        	logWatcher.watchAndAlert();
+        	TimeUnit.SECONDS.sleep(1);
+        }
+
+        
+//        assertEquals("Notifying robert glaser@cas.de: ExceptionCode 42", outContent.toString());
     }
 }
